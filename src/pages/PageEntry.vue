9e4cc4f321ec7efad3cb40a48478bd5f8b422242
Simon Eldevig <simoneldevig@gmail.com>
2019-12-22 13:00:15 +0100

    Add faker.js text

--- src/pages/PageEntry.vue
+++ src/pages/PageEntry.vue
@@ -3,14 +3,17 @@
     <el-row class="tac" :gutter="20">
       <el-col :span="16">
         <div class="flex items-center justify-between">
-          <h1><span class="text-capitalize">{{ id }}</span> model</h1>
           <div>
+            <h1 class="mb0"><span class="text-capitalize">{{ id }}</span> model</h1>
+            <small class="block mb3">To use values from faker.js, simply insert faker.js functions into the inputs. E.g. faker.name.findName(); <br>docs can be found here: <a href="https://github.com/marak/Faker.js/">https://github.com/marak/Faker.js</a></small>
+          </div>
+          <!-- <div>
             <el-tag class="ml1" type="success">/GET</el-tag>
             <el-tag class="ml1" type="primary">/PUSH</el-tag>
             <el-tag class="ml1" type="warning">/PATCH</el-tag>
             <el-tag class="ml1" type="info">/PUT</el-tag>
             <el-tag class="ml1" type="danger">/DELETE</el-tag>
-          </div>
+          </div> -->
         </div>
 
         <collapse v-if="entry[id]" :id="id" :data="entry[id]" :title="id" :index="0" :is-sub-child="false" />
