ef9a82aab53197869c623a486e995978b6de7902
Simon Eldevig <sne@novicell.dk>
2021-04-08 13:11:25 +0200

    Fix install issues

--- postinstall.js
+++ postinstall.js
@@ -4,5 +4,9 @@ const copyfiles = require('copyfiles');
 const outDir = path.resolve(process.env.INIT_CWD);
 
 if (process.cwd() !== outDir) {
-  copyfiles(['./json-gui/**/*.json', outDir]);
+  copyfiles(['./json-gui/**/*.json', outDir], { error: false, verbose: false }, function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
