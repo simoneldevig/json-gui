770bbc61728f8b807d24873c791a204185ccc914
Simon Eldevig <sne@novicell.dk>
2021-05-19 10:20:44 +0200

    fix: do not override files if they exist on install

--- postinstall.js
+++ postinstall.js
@@ -4,7 +4,7 @@ const copyfiles = require('copyfiles');
 const outDir = path.resolve(process.env.INIT_CWD);
 
 if (process.cwd() !== outDir) {
-  copyfiles(['./json-gui/**/*.json', outDir], { error: false, verbose: false }, function (err) {
+  copyfiles(['./json-gui/**/*.json', outDir], { error: false, verbose: false, soft: true }, function (err) {
     if (err) {
       console.error(err);
     }
